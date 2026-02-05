b6c282142a586d212b140e5e4683bc67b560af2c
Dhruv <vaishnavdhruv1237@gmail.com>
2026-02-05 08:11:48 +0530

    [PU][added two files]

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import TaskList from "../components/TaskList";
 
 const Dashboard = () => {
   const navigate = useNavigate();
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/tasks");
+      const data = response.json();
+      setTasks(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const handleLogout = () => {
     localStorage.removeItem("loginData");
